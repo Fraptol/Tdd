e17f8e77e1b1183f7ccdc810d94e396c8a514fa9
Fraptol <DiakMatter@tutanota.com>
2022-10-24 22:39:05 +0200

    0.1

--- src/Koszonto/Koszonto.java
+++ src/Koszonto/Koszonto.java
@@ -1,12 +1,17 @@
 
 package Koszonto;
 
-//0.1
+//0.2
 public class Koszonto {
     public String Reply(String name) {
         if (name==null) {
             System.out.println("Hello, my friend.");
         }
+
+        else if (name.matches("Bob")){
+            System.out.println("Hello, "+ "Bob" + ".");
+        return "Hello, "+ "Bob" + ".";}
+
         return "Hello, my friend.";
     }
 }
