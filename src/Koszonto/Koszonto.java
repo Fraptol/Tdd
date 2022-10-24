fccced904bfb35b818daa95db8deef01ab28bc74
Fraptol <DiakMatter@tutanota.com>
2022-10-24 22:44:10 +0200

    0.1

--- src/Koszonto/Koszonto.java
+++ src/Koszonto/Koszonto.java
@@ -1,17 +1,16 @@
 
 package Koszonto;
 
-//0.2
+//0.3
 public class Koszonto {
     public String Reply(String name) {
-        if (name==null) {
+        if (name == null) {
             System.out.println("Hello, my friend.");
+            return "Hello, my friend.";
+        } else if (name.matches("Bob")) {
+            System.out.println("Hello, " + "Bob" + ".");
+            return "Hello, " + "Bob" + ".";
         }
-
-        else if (name.matches("Bob")){
-            System.out.println("Hello, "+ "Bob" + ".");
-        return "Hello, "+ "Bob" + ".";}
-
-        return "Hello, my friend.";
+        return "Failure";
     }
 }
--- src/Koszonto/Teszt.java
+++ src/Koszonto/Teszt.java
@@ -3,9 +3,14 @@ import static org.junit.Assert.assertEquals;
 
 public class Teszt {
 
+    String nameless ="";
+    String reply0 ="Hello, my friend.";
     String names1 = new String ("Bob");
     String reply1 = new String("Hello, Bob.");
 
+    public void shouldReplyCorrectly0(String nameless) {
+        assertEquals(reply0, new Koszonto().Reply(nameless));
+    }
     public void shouldReplyCorrectly1(String names) {
         assertEquals(reply1, new Koszonto().Reply(names1));
     }
