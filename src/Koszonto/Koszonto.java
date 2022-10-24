3ec2f4fd8a219e0590e8403ee1048eec934adb89
Fraptol <DiakMatter@tutanota.com>
2022-10-24 23:43:11 +0200

    0.1.1

--- src/Koszonto/Koszonto.java
+++ src/Koszonto/Koszonto.java
@@ -1,15 +1,22 @@
 
 package Koszonto;
 
+import java.net.ConnectException;
+
 //0.3
 public class Koszonto {
-    public String Reply(String name) {
-        if (name == null) {
+    public String Reply(String[] names) {
+        final int length= names.length;
+        if (length == 0) {
             System.out.println("Hello, my friend.");
             return "Hello, my friend.";
-        } else if (name.matches("Bob")) {
-            System.out.println("Hello, " + name + ".");
-            return "Hello, " + name + ".";
+        } else if (length>0) {
+            //String[] nametext=new String[]{names.split(" ")};
+            String text="";
+            for (int i = 0; i < names.length; i++)
+            {text= text+names[i];}
+            System.out.println("Hello, " + text + ".");
+            return "Hello, " + names + ".";
         }
         return "I failed.";
     }
--- src/Koszonto/Teszt.java
+++ src/Koszonto/Teszt.java
@@ -9,11 +9,13 @@ public class Teszt {
     String reply1 = "Hello, Bob.";
 
 
-
     public void shouldReplyCorrectly0(String nameless) {
-        assertEquals(reply0, new Koszonto().Reply(nameless));
+        assertEquals(reply0, new Koszonto().Reply(new String[] {nameless}));
     }
     public void shouldReplyCorrectly1(String names1) {
-        assertEquals(reply1, new Koszonto().Reply(names1));
+        assertEquals(reply1, new Koszonto().Reply(new String[] {names1}));
     }
-}
+
+    String[] names2 = new String[] {"Alice", "Bob", "Jerry"};
+    String reply2 = new String("Hello, Alice, Bob, and Jerry.");
+    }
